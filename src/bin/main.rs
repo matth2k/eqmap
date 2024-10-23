caef103f2bde6da108d665379229cf3c0aff8940
matth2k <mrh259@cornell.edu>
2024-10-23 14:36:05 -0400

    update examples

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,10 @@
-use std::io::Read;
-
+use clap::Parser;
 use egg::*;
 use lut_synth::{cost::LUTKCostFn, lut, rewrite::all_rules};
+use std::{
+    io::{IsTerminal, Read},
+    path::PathBuf,
+};
 
 /// simplify expr using egg, and pretty print it back out
 fn simplify_expr(
@@ -67,13 +70,41 @@ fn test_eval() {
 // #[test]
 // fn test_dsd() {
 //     let expr: RecExpr<lut::LutLang> = "(MUX s1 (MUX s0 a b) (MUX s0 c d))".parse().unwrap();
-//     let other: RecExpr<lut::LutLang> = "(LUT 17858377336630846080 s1 s0 a b c d)".parse().unwrap();
+//     let other: RecExpr<lut::LutLang> = "(LUT 18374951396690406058 s1 s0 a b c d)".parse().unwrap();
+//     let dsd: RecExpr<lut::LutLang> = "(LUT 1337 s1 (LUT 61642 ?s1 ?s0 ?c ?d) a b)"
+//         .parse()
+//         .unwrap();
 //     assert!(lut::LutLang::func_equiv_always(&expr, &other));
+//     assert!(lut::LutLang::func_equiv_always(&other, &dsd));
 // }
 
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to input file. If not provided, reads from stdin
+    input: Option<PathBuf>,
+
+    /// Verify functionality of the output
+    #[arg(short, long, default_value_t = false)]
+    no_verify: bool,
+}
+
 fn main() -> std::io::Result<()> {
+    let args = Args::parse();
     let mut buf = String::new();
-    std::io::stdin().read_to_string(&mut buf)?;
+
+    if args.input.is_some() {
+        std::fs::File::open(args.input.unwrap())?.read_to_string(&mut buf)?;
+    } else {
+        let mut stdin = std::io::stdin();
+        if stdin.is_terminal() {
+            while stdin.read_line(&mut buf)? <= 1 {}
+        } else {
+            stdin.read_to_string(&mut buf)?;
+        }
+    }
+
     for line in buf.lines() {
         let line = line.trim();
         if line.starts_with("//") || line.is_empty() {
@@ -85,7 +116,9 @@ fn main() -> std::io::Result<()> {
         println!("{} => {}", expr.to_string(), simplified.to_string());
 
         // Verify functionality
-        if true {
+        if !args.no_verify {
+            eprintln!("Skipping functonality tests...");
+        } else {
             let result = lut::LutLang::func_equiv_always(&expr, &simplified);
             if !result {
                 eprintln!("Failed for explanation {:?}", expl.to_string());
