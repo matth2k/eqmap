ca6864b878abc69ef0b74c12dd3be2ce0be71811
jeremy ku-benjet <jeremykubenjetschool@gmail.com>
2024-10-30 13:40:46 -0400

    Implement LUT counting functions.

--- src/cost.rs
+++ src/cost.rs
@@ -50,3 +50,66 @@ impl CostFunction<LutLang> for KLUTCostFn {
         })
     }
 }
+
+/// The size of a given LUT.
+pub enum LutSize {
+    /// A LUT of given size.
+    Size(usize),
+    /// A LUT of any size. A wildcard in a sense.
+    Any,
+}
+
+/// A cost function counting LUTs of a given size.
+pub struct NumKLUTsCostFn {
+    size: LutSize,
+}
+
+impl NumKLUTsCostFn {
+    /// Returns a new cost function counting LUTs of `size`.
+    pub fn new(size: LutSize) -> Self {
+        const TOO_LARGE: usize = LutLang::MAX_LUT_SIZE + 1;
+        match size {
+            LutSize::Size(0) | LutSize::Size(TOO_LARGE..) => {
+                panic!("k must be between 1 and {}", LutLang::MAX_LUT_SIZE)
+            }
+            size => Self { size },
+        }
+    }
+}
+
+impl CostFunction<LutLang> for NumKLUTsCostFn {
+    type Cost = u64;
+    fn cost<C>(&mut self, enode: &LutLang, mut costs: C) -> Self::Cost
+    where
+        C: FnMut(Id) -> Self::Cost,
+    {
+        let op_cost = match enode {
+            LutLang::Lut(l) => match self.size {
+                LutSize::Size(k) if l.len() == k + 1 => 1,
+                LutSize::Any => 1,
+                LutSize::Size(_) => 0,
+            },
+            LutLang::Program(_)
+            | LutLang::Const(_)
+            | LutLang::Var(_)
+            | LutLang::DC
+            | LutLang::Nor(_)
+            | LutLang::Mux(_)
+            | LutLang::And(_)
+            | LutLang::Xor(_)
+            | LutLang::Not(_) => 0,
+        };
+        enode.fold(op_cost, |sum, id| sum + costs(id))
+    }
+}
+
+/// Return the number of luts in the given expr.
+pub fn get_lut_count(expr: &RecExpr<LutLang>) -> u64 {
+    NumKLUTsCostFn::new(LutSize::Any).cost_rec(expr)
+}
+
+/// Return the number of k-luts in the given expr.
+pub fn get_lut_count_k(expr: &RecExpr<LutLang>, k: usize) -> u64 {
+    let size = LutSize::Size(k);
+    NumKLUTsCostFn::new(size).cost_rec(expr)
+}
