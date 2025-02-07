e9136e52d1d887eebe49935458baa54b0eb961ec
berkgokmen14 <113947067+berkgokmen14@users.noreply.github.com>
2025-02-07 14:04:02 -0500

    Added lvv-vivado (#96)
    Co-authored-by: matth2k <mrh259@cornell.edu>

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+PWDL=$(pwd)
+IDIR=$(realpath $(dirname $BASH_SOURCE)/..)
+cd $IDIR
 cargo build --release --features default,$1,$2,$3,$4
 
 chmod +x ./bin/*
 chmod +x ./utils/*.py
-export PATH=$PWD/target/release:$PWD/bin:$PWD/utils:$PATH
-
+export PATH=$IDIR/target/release:$IDIR/bin:$IDIR/utils:$PATH
+cd $PWDL
 echo "SUCCESS"
