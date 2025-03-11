9d672d987af977ed01d7f7951524b538e5467d39
Matt Hofmann <mrh259@cornell.edu>
2025-03-11 16:37:54 -0400

    Fix setup script for when build fails (#124)

--- utils/setup.sh
+++ utils/setup.sh
@@ -2,10 +2,15 @@
 PWDL=$(pwd)
 IDIR=$(realpath $(dirname $BASH_SOURCE)/..)
 cd $IDIR
-cargo build --release --features default,$1,$2,$3,$4
 
-chmod +x ./bin/*
-chmod +x ./utils/*.py
-export PATH=$IDIR/target/release:$IDIR/bin:$IDIR/utils:$PATH
-cd $PWDL
-echo "SUCCESS"
+if cargo build --release --features default,$1,$2,$3,$4; then
+
+    chmod +x ./bin/*
+    chmod +x ./utils/*.py
+    export PATH=$IDIR/target/release:$IDIR/bin:$IDIR/utils:$PATH
+    cd $PWDL
+    echo "SUCCESS"
+else
+    cd $PWDL
+    echo "FAILURE"
+fi
