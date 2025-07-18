743ef80d6c28217ae5099a6dc7400bec4dc864db
Matthew Hofmann <mrh259@cornell.edu>
2025-07-17 20:38:24 -0400

    Update license (#159)

--- verilog/stdcells.v
+++ verilog/stdcells.v
@@ -1,5 +1,30 @@
 // Based on the NanGate 45nm Open Cell Library
 // https://mflowgen.readthedocs.io/en/latest/stdlib-freepdk45.html
+//
+// ******************************************************************************
+// *                                                                            *
+// *                   Copyright (C) 2004-2010, Nangate Inc.                    *
+// *                           All rights reserved.                             *
+// *                                                                            *
+// * Nangate and the Nangate logo are trademarks of Nangate Inc.                *
+// *                                                                            *
+// * All trademarks, logos, software marks, and trade names (collectively the   *
+// * "Marks") in this program are proprietary to Nangate or other respective    *
+// * owners that have granted Nangate the right and license to use such Marks.  *
+// * You are not permitted to use the Marks without the prior written consent   *
+// * of Nangate or such third party that may own the Marks.                     *
+// *                                                                            *
+// * This file has been provided pursuant to a License Agreement containing     *
+// * restrictions on its use. This file contains valuable trade secrets and     *
+// * proprietary information of Nangate Inc., and is protected by U.S. and      *
+// * international laws and/or treaties.                                        *
+// *                                                                            *
+// * The copyright notice(s) in this file does not indicate actual or intended  *
+// * publication of this file.                                                  *
+// *                                                                            *
+// *      NGLibraryCharacterizer, Development_version - build 201012062042      *
+// *                                                                            *
+// ******************************************************************************
 
 module AND2_X1 (
     A1,
